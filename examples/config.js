0e29fdb1028b23fb7e1320ab6f6494c4e5fc6b7a
Matt Hoyle <code@deployable.co>
2018-03-20 22:18:13 +0000

    help and version, not sure how they fit in config

--- examples/config.js
+++ examples/config.js
@@ -1,19 +1,19 @@
 //const largs = require('largs')
 const largs = require('../')
 
-largs.options({
-  first: {
-    short: 'f',
-    long: 'firstthing',
+largs.help().version('0.2.0').options({
+  f: {
     required: true,
+    help: 'This is the f description for help',
   },
 
   second: {
     long: 'two',
     type: 'integer',
     default: 1,
+    help: 'Some more help for the two integer',
   },
-
+ 
 })
 
 const args = largs.run() // returns largs options`
